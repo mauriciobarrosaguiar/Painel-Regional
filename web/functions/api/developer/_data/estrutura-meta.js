322ffaa738e03a60396f716e70fbc8fba86cd2bb
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-07-28 20:17:24 -0300

    Registra metadados da Estrutura de Pessoas interna

---
+++ web/functions/api/developer/_data/estrutura-meta.js
@@ -0,0 +1,1 @@
+export default {"nome_arquivo":"Estrutura de Pessoas - Casa Gx 2026.xlsx","nome_planilha":"Mês Atual","linhas":590,"regionais":37,"distritais":100,"consultores":323,"carregado_em":"2026-07-28"}
